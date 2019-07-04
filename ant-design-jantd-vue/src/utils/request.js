ea705c04aeb056c8e9c9400595bfd4c688c5ff84
xiaguifu <xiaguifu@mksoftapp.com>
2019-07-04 09:34:38 +0800

    refs #15284 涉及到jeecg修改为jantd重构

--- ant-design-jantd-vue/src/utils/request.js
+++ ant-design-jantd-vue/src/utils/request.js
@@ -7,7 +7,7 @@ import { ACCESS_TOKEN } from "@/store/mutation-types"
 
 // 创建 axios 实例
 const service = axios.create({
-  baseURL: '/jeecg-boot', // api base_url
+  baseURL: '/jantd-boot', // api base_url
   timeout: 6000 // 请求超时时间
 })
 
--- ant-design-jantd-vue/vue.config.js
+++ ant-design-jantd-vue/vue.config.js
@@ -62,7 +62,7 @@ module.exports = {
           '/jeecg-boot': ''  //默认所有请求都加了jeecg-boot前缀，需要去掉
         }
       },*/
-      '/jeecg-boot': {
+      '/jantd-boot': {
         target: 'http://localhost:8080', //请求本地 需要jeecg-boot后台项目
         ws: false,
         changeOrigin: true
