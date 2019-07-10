0cd3c115b68b75260ae5b35ecb527f438227fdac
xiaguifu <xiaguifu@mksoftapp.com>
2019-07-10 15:44:25 +0800

    refs #15527 前后端文件名涉及到jeecg修改为jantd

--- ant-design-jantd-vue/vue.config.js
+++ ant-design-jantd-vue/vue.config.js
@@ -54,16 +54,8 @@ module.exports = {
   devServer: {
     port: 3000,
     proxy: {
-     /* '/api': {
-        target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro', //mock API接口系统
-        ws: false,
-        changeOrigin: true,
-        pathRewrite: {
-          '/jeecg-boot': ''  //默认所有请求都加了jeecg-boot前缀，需要去掉
-        }
-      },*/
       '/jantd-boot': {
-        target: 'http://localhost:8080', //请求本地 需要jeecg-boot后台项目
+        target: 'http://localhost:8080', //请求本地 需要jantd-boot后台项目
         ws: false,
         changeOrigin: true
       },
